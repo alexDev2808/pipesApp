b4d054ccff63b164689c43c26d0dbd74727037c9
alexDev2808 <colores.2808.8000@gmail.com>
2024-02-09 23:28:58 -0600

    agregando primeflex

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BasicsPageComponent {
 
+  public nameLower: string = 'ALEXIS';
+  public nameUpper: string = 'alexis';
+  public nameTitle: string = 'aLEXis';
+
 }

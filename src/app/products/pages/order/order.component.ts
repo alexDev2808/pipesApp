6d7d65a9e592af7ea1157f3f79d6f889417e0e4a
alexDev2808 <colores.2808.8000@gmail.com>
2024-02-15 18:17:43 -0600

    Pipe: canFly

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'products-order',
@@ -9,9 +10,31 @@ import { Component } from '@angular/core';
 export class OrderComponent {
 
   public isUpperCase: boolean = false;
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.blue
+    },
+    {
+      name: 'Batman',
+      canFly: false,
+      color: Color.black
+    },
+    {
+      name: 'Linterna Verde',
+      canFly: true,
+      color: Color.green
+    },
+    {
+      name: 'Iron Man',
+      canFly: true,
+      color: Color.red
+    },
+  ]
 
   toggleUpperCase(): void {
     this.isUpperCase = !this.isUpperCase;
   }
-  
+
 }
